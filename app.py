48848c4d5655a749404573043fbb618ec99c68c9
Epoch10 <s.crompton50@ntlworld.com>
2024-12-23 19:22:21 +0000

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,6 @@ with st.container():
 # Documentation
 contact_form = """
 <form action="https://formsubmit.co/s.crompton50@ntlworld.com" method="POST">
-    <input type="hidden" name="_captcha" value="false">
     <input type="text" name="name" placeholder="Your name" required>
     <input type="email" name="email" placeholder="Your email" required>
     <textarea name="message" placeholder="Your message here" required></textarea>
