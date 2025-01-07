6764958d5f465621665b747dd6f1dfe69c40ed3c
Epoch10 <s.crompton50@ntlworld.com>
2025-01-07 13:01:03 +0000

    Update app.py

--- app.py
+++ app.py
@@ -70,6 +70,16 @@ text-align: center;
 """
 st.markdown(footer,unsafe_allow_html=True)
 
+st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 1rem;
+                    padding-bottom: 0rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
 
 with st.container():
     custom_html = """
